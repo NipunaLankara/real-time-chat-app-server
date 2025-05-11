489278bf5a58e8bb333404b3cceb3ebe67aad07d
NipunaLankara <nipunalankara@gmail.com>
2025-05-11 12:14:34 +0530

    1

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors: { origin: "http://localhost:5173" }
+    cors: { origin: '*', }
 });
 
 const users = new Map(); // socket.id -> username
